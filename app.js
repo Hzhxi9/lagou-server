568ea9f07c5c2f97677a45a8aa16cbf4cabf70bb
huangzx <48216476+Hzhxi9@users.noreply.github.com>
2021-11-01 12:04:25 +0800

    fixed

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.disable('etag');
 app.all('*', function (req, res, next) {
 
   res.header("Access-Control-Allow-Origin", req.headers.origin);
-  // res.header('Access-Control-Allow-Credentials', true);//告诉客户端可以在HTTP请求中带上Cookie
+  res.header('Access-Control-Allow-Credentials', true);//告诉客户端可以在HTTP请求中带上Cookie
   res.header("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, " +
     "Last-Modified, Cache-Control, Expires, Content-Type, Content-Language, Cache-Control, X-E4M-With,X_FILENAME");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
